3e2b119a072a97c98f0b6b1e56a4b63608ad19ec
Patriv06 <privas06@gmail.com>
2023-01-12 21:20:21 -0300

    para que deploye

--- src/main/java/com/back/carreras/model/Autodromo.java
+++ src/main/java/com/back/carreras/model/Autodromo.java
@@ -2,19 +2,14 @@
 package com.back.carreras.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 
-import java.util.Set;
-import javax.persistence.CascadeType;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import lombok.Getter;
+
 import lombok.Setter;
 
 @Getter @Setter
